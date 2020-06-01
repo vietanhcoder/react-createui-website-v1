9924a830b26bc4422ed15a1a7f65c987900fd05a
Viet Anh Le <mc.vietanh@gmail.com>
2020-06-01 09:33:35 +0700

    working on navbar

--- src/containers/LayoutPublic/Navbar.js
+++ src/containers/LayoutPublic/Navbar.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
+
+// tạo 1 cái style component chứa cái Navlinklv1
+// Tạo 1 cái style component chứa Navlinklv2
+
+// Cấu trúc:
+/**
+ * Navbarwrapper
+ *  container
+ *   - logo
+ *   - Lv1NavLink: how it works
+ *   - Lv1NavLink: postpay for business (dropdown)
+ *       dropdown-wrapper
+ *         Lv2NavLink: benefits
+ *         Lv2NavLink: benefits
+ *   - Login button
+ *
+ */
 
 const Navbar = () => {
   return <div>this is navbar content</div>;
